1c5b59fa369ace475852110e7140af9acce5db1c
Max Hillaert <789876+maxhillaert@users.noreply.github.com>
2022-12-02 23:57:08 +0000

    add

--- app/transcribe.py
+++ app/transcribe.py
@@ -1,10 +1,13 @@
 """Blabl"""
 #%%
+import pathlib
 from util.download import download_if_not_exist
 
 
+working_dir = pathlib.Path().resolve()
+
 URL = 'https://media.blubrry.com/takeituneasy/content.blubrry.com/takeituneasy/lex_ai_andrej_karpathy.mp3'
 
-download_if_not_exist(URL, '../data/lex_ai_andrej_karpathy.mp3')
+download_if_not_exist(URL, f'{working_dir}/data/lex_ai_andrej_karpathy.mp3')
 
 #%%s
--- app/util/download.py
+++ app/util/download.py
@@ -2,7 +2,7 @@
 
 import os
 import requests
-
+from tqdm import tqdm
 
 def download_if_not_exist(url: str, filename: str) -> None:
     """Download a file if it does not exist
@@ -12,7 +12,16 @@ def download_if_not_exist(url: str, filename: str) -> None:
     """
     if not os.path.exists(filename):
         response = requests.get(url, allow_redirects=True, timeout=60)
-        print(f"Downloading {filename}")
-        open(filename, 'wb').write(response.content)
+        total = int(response.headers.get('content-length', 0))
+        with open(filename, 'wb') as file, tqdm(
+                desc=filename,
+                total=total,
+                unit='iB',
+                unit_scale=True,
+                unit_divisor=1024,
+        ) as prog_bar:
+            for data in response.iter_content(chunk_size=1024):
+                size = file.write(data)
+                prog_bar.update(size)
     else:
         print(f"File {filename} already exits")
